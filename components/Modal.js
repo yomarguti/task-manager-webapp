218bfcf2216f39567207c9a0cc2768be5499f6bd
Yomar Gutierrez Mejia <yomar.guti@gmail.com>
2021-09-10 10:00:20 -0500

    Added handler to backdrop to dismiss the modal when clicking on it

--- components/Modal.js
+++ components/Modal.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 
 const Modal = ({ show, onClose, children, title }) => {
   const [isBrowser, setIsBrowser] = useState(false);
+  const modalRef = useRef(null);
 
   useEffect(() => {
     setIsBrowser(true);
+    const handler = (e) => {
+      if (!modalRef?.current?.contains(e.target)) {
+        onClose();
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
   }, []);
 
   const handleCloseClick = (e) => {
@@ -14,8 +25,11 @@ const Modal = ({ show, onClose, children, title }) => {
   };
 
   const modalContent = show ? (
-    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-screen bg-black bg-opacity-50">
-      <div className="relative h-40 bg-white border border-gray-400 w-96 rounded-xl">
+    <div className="absolute top-0 flex items-center justify-center w-full transition bg-black bg-opacity-50 -bottom-40">
+      <div
+        ref={modalRef}
+        className="relative h-40 bg-white border border-gray-400 w-96 rounded-xl"
+      >
         <h1 className="pt-4 text-lg font-bold text-center">{title}</h1>
         <a
           href="#"
